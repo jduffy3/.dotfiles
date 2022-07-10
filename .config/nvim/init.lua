8854380ec9872fb4a692dbf490939233b6454e38
James Duffy <jduffy3@gmail.com>
2022-07-10 10:31:44 -0400

    Update spacing for tabs ala vimrc

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,6 +9,11 @@ vim.opt.relativenumber = true
 vim.opt.scrolloff = 8
 vim.opt.updatetime = 100
 vim.api.nvim_command("set noswapfile")
+vim.opt.expandtab = true
+vim.opt.tabstop = 2 
+vim.opt.softtabstop = 2 
+vim.opt.shiftwidth = 2 
+vim.opt.shiftround = true
 
 function my_statusline()
   local branch = vim.fn.FugitiveHead()
