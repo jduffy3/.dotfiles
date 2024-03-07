bd36be6ca2464617b49d112c36462985aa8b3d7d
James Duffy <jduffy3@gmail.com>
2024-03-07 12:18:08 -0500

    run ruby test file from impl or test file using nvim apis :)

--- .config/nvim/after/ftplugin/ruby.lua
+++ .config/nvim/after/ftplugin/ruby.lua
@@ -5,7 +5,6 @@ end
 -- let b:did_ftplugin = 1
 vim.b.did_ftplugin = 1
 
-vim.keymap.set("n", "<leader>rt", ":!rails test %<CR>", { desc = "[R]un [T]estfile" })
 vim.keymap.set("n", "<leader>rct", function()
 	local current_line_number = vim.fn.line(".")
 	vim.api.nvim_command(":!rails test %" .. ":" .. current_line_number)
@@ -13,4 +12,16 @@ end, { desc = "[R]un [C]urrent [T]est" })
 vim.keymap.set("n", "<leader>nt", function()
 	vim.api.nvim_command(":/test ")
 end, { desc = "[N]ext [T]est" })
+
 vim.keymap.set("n", "<leader>tf", ":e %:r:s?app?test?_test.rb<CR>", { desc = "Open [T]est [F]ile" })
+
+-- run test from main file or test file
+vim.keymap.set("n", "<leader>rt", function()
+	local test_file_name = string.gsub(vim.fn.expand("%"), "^app/", "test/")
+	if test_file_name:sub(-#"_test.rb") == "_test.rb" then
+		vim.api.nvim_command(":! rails test " .. test_file_name)
+	else
+		test_file_name = string.gsub(test_file_name, "%.rb", "_test.rb")
+		vim.api.nvim_command(":! rails test " .. test_file_name)
+	end
+end, { desc = "[R]un [T]estfile" })
