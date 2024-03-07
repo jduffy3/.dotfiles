829b86cd084319e5325bd0f190d8699cc8342d39
James Duffy <jduffy3@gmail.com>
2024-03-06 23:26:27 -0500

    update ruby keymaps

--- .config/nvim/after/ftplugin/ruby.lua
+++ .config/nvim/after/ftplugin/ruby.lua
@@ -4,9 +4,13 @@ if vim.fn.exists("b:did_ftplugin") == 0 then
 end
 -- let b:did_ftplugin = 1
 vim.b.did_ftplugin = 1
-vim.keymap.set("n", "<leader>rt", ":!rails test %<CR>", { desc = "[R]un [T]estfile" }) -- run test file
+
+vim.keymap.set("n", "<leader>rt", ":!rails test %<CR>", { desc = "[R]un [T]estfile" })
 vim.keymap.set("n", "<leader>rct", function()
 	local current_line_number = vim.fn.line(".")
 	vim.api.nvim_command(":!rails test %" .. ":" .. current_line_number)
-end, { desc = "[R]un [C]urrent [T]est" }) -- run test file
-vim.keymap.set("n", "<leader>tf", ":e %:r:s?app?test?_test.rb<CR>", { desc = "[T]est [F]ile" }) -- open test file
+end, { desc = "[R]un [C]urrent [T]est" })
+vim.keymap.set("n", "<leader>nt", function()
+	vim.api.nvim_command(":/test ")
+end, { desc = "[N]ext [T]est" })
+vim.keymap.set("n", "<leader>tf", ":e %:r:s?app?test?_test.rb<CR>", { desc = "Open [T]est [F]ile" })
